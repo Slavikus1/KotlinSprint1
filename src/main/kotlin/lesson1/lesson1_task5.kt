0543a658a53a51e1d85bb055cf32461163e116d1
Slavikus1 <vyacheslav921@icloud.com>
2024-03-09 16:58:52 +0300

    KS-1-5 tried to complete the task in a right way!

---
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -0,0 +1,16 @@
+package lesson1
+
+fun main(){
+   val seconds = 6480
+   val minutes = seconds/60
+   val hours = seconds/3600
+   val rest = 0.8 * 60
+
+
+   println(seconds)
+   println(minutes)
+   println(hours)
+   println(rest)
+
+   println("Гагарин был в космосе 01:48:00")
+}
