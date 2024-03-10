e6f2f0af7bb8ddaa9c2634136df8d4bd87fd5d2d
Slavikus1 <vyacheslav921@icloud.com>
2024-03-11 00:06:53 +0300

    KS-2-2-refactor salaries

--- src/main/kotlin/lesson2/lesson2_task2.kt
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -1,12 +1,17 @@
 package lesson2
 
 fun main() {
-    val salaryOfSeniors = 50 * 30000
-    val salaryOfJuniors = 30 * 20000
-    val totalExpenses = salaryOfJuniors + salaryOfSeniors
-    val averageSalary = totalExpenses / 80
+    val numberOfSeniors = 50
+    val numberOfJuniors = 30
+    val numberOFAllWorkers = numberOfJuniors + numberOfSeniors
+    val salaryOfJuniors = 20000
+    val salaryOfSeniors = 30000
+    val expensesOnSeniors = numberOfSeniors * salaryOfSeniors
+    val expenseOfJuniors = numberOfJuniors * salaryOfJuniors
+    val totalExpenses = expenseOfJuniors + expensesOnSeniors
+    val averageSalary = totalExpenses / numberOFAllWorkers
 
-    println(salaryOfSeniors)
+    println(expensesOnSeniors)
     println(totalExpenses)
     println(averageSalary)
 }
