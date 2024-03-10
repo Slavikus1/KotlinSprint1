5e123bbb31195d8eb46645a9618acfbc9315a727
Slavikus1 <vyacheslav921@icloud.com>
2024-03-10 23:43:40 +0300

    KS-1-5-used % operator, hope its correct

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,16 +1,18 @@
 package lesson1
 
-fun main(){
-   val seconds = 6480
-   val minutes = seconds/60
-   val hours = seconds/3600
-   val rest = 0.8 * 60
-
-
-   println(seconds)
-   println(minutes)
-   println(hours)
-   println(rest)
-
-   println("Гагарин был в космосе 01:48:00")
+fun main() {
+    val allSeconds = 6480
+    val findMinutes = 60
+    val findHours = 3600
+    val allMinutes = allSeconds / findMinutes
+    //println(allMinutes)
+    val spaceHours = allSeconds / findHours
+    //println(hours)
+    val restOfSecondsAndMinutes = allSeconds % findHours
+    //println(restOfSecondsAndMinutes)
+    val spaceSec = restOfSecondsAndMinutes % findMinutes
+    //println(spaceSec)
+    val spaceMin = restOfSecondsAndMinutes / findMinutes
+    //println(spaceMin)
+    println("Гагарин провел в космосе [01:$spaceMin:0$spaceSec]")
 }
