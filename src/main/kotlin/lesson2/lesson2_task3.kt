9663e1b1242b04ac689c969427226aa43a860409
Slavikus1 <vyacheslav921@icloud.com>
2024-03-10 14:27:58 +0300

    KS-2-3 trains

---
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -0,0 +1,9 @@
+package lesson2
+
+fun main() {
+    val timeStart = 39 + (9 * 60)
+    val timeTravel = 457
+    val timeFinish = (timeStart + timeTravel) / 60.0
+    //println(timeFinish)
+    println("Поезд приезжает в 17:26")
+}
