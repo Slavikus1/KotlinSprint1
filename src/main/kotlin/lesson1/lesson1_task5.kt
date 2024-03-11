6c2ae10ff926a47cef785783d06327641b51a4c4
Slavikus1 <vyacheslav921@icloud.com>
2024-03-11 22:55:34 +0300

    KS-1-5 used String.format and consts

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,18 +1,21 @@
 package lesson1
 
+const val ALL_SECONDS = 6480
+const val FIND_MINUTES = 60
+
 fun main() {
-    val allSeconds = 6480
-    val findMinutes = 60
+
     val findHours = 3600
-    val allMinutes = allSeconds / findMinutes
+    val allMinutes = ALL_SECONDS / FIND_MINUTES
     //println(allMinutes)
-    val spaceHours = allSeconds / findHours
-    //println(hours)
-    val restOfSecondsAndMinutes = allSeconds % findHours
+    val spaceHours = ALL_SECONDS / findHours
+//    println(spaceHours)
+    val restOfSecondsAndMinutes = ALL_SECONDS % findHours
     //println(restOfSecondsAndMinutes)
-    val spaceSec = restOfSecondsAndMinutes % findMinutes
-    //println(spaceSec)
-    val spaceMin = restOfSecondsAndMinutes / findMinutes
-    //println(spaceMin)
-    println("Гагарин провел в космосе [01:$spaceMin:0$spaceSec]")
+    val spaceSec = restOfSecondsAndMinutes % FIND_MINUTES
+//    println(spaceSec)
+    val spaceMin = restOfSecondsAndMinutes / FIND_MINUTES
+//    println(spaceMin)
+    val stringFormat = String.format("%02d:%d:%02d", spaceHours, spaceMin, spaceSec)
+    println(stringFormat)
 }
