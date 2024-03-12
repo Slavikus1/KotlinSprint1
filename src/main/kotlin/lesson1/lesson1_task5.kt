ca42900055cffba32bf1b00cfc2f86bd580150f4
Slavikus1 <vyacheslav921@icloud.com>
2024-03-12 11:26:50 +0300

    KS-1-5-deleted comments

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -7,15 +7,10 @@ fun main() {
 
     val findHours = 3600
     val allMinutes = ALL_SECONDS / FIND_MINUTES
-    //println(allMinutes)
     val spaceHours = ALL_SECONDS / findHours
-//    println(spaceHours)
     val restOfSecondsAndMinutes = ALL_SECONDS % findHours
-    //println(restOfSecondsAndMinutes)
     val spaceSec = restOfSecondsAndMinutes % FIND_MINUTES
-//    println(spaceSec)
     val spaceMin = restOfSecondsAndMinutes / FIND_MINUTES
-//    println(spaceMin)
     val stringFormat = String.format("%02d:%d:%02d", spaceHours, spaceMin, spaceSec)
     println(stringFormat)
 }
