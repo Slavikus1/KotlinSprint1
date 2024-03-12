d47cf277052ce690ab113f9e5ce4916cd43fd8b9
Slavikus1 <vyacheslav921@icloud.com>
2024-03-12 18:12:31 +0300

    KS-2-5-refactor consts

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -2,11 +2,10 @@ package lesson2
 
 import kotlin.math.pow
 
-const val MAIN_SUMM = 70000.0
-const val PERCENT_PER_YEAR = 16.7
-const val YEARS = 20.0
-
 fun main() {
-    val result = MAIN_SUMM * (1 + (PERCENT_PER_YEAR / 100)).pow(YEARS)
+    val mainSUmm = 70000.0
+    val percentPerYear = 16.7
+    val years = 20.0
+    val result = mainSUmm * (1 + (percentPerYear / 100)).pow(years)
     println(String.format("%6.3f", result))
 }
