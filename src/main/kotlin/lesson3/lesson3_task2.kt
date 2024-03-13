d64d123b9d02881b97130a779911998b7f697f87
Slavikus1 <vyacheslav921@icloud.com>
2024-03-13 19:29:14 +0300

    KS-3-2 refactor

--- src/main/kotlin/lesson3/lesson3_task2.kt
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -1,16 +1,15 @@
 package lesson3
 
 fun main() {
-    val familyName = "Андреева"
+    var familyName = "Андреева"
     val name = "Татьяна"
     val surname = "Сергеевна"
-    val age = 20
-    val nextFamilyName = "Сидорова"
-    val age2 = 22
-    println(
-        """
-        [$familyName $name $surname, $age.]
-        [$nextFamilyName $name $surname, $age2.]
-    """.trimIndent()
-    )
+    var age = 20
+
+    println("$familyName $name $surname, $age.")
+
+    familyName = "Сидорова"
+    age = 22
+
+    println("$familyName $name $surname, $age.")
 }
