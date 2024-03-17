5b6efb8d0fa494d371921055c6e17b2f068b9767
Slavikus1 <vyacheslav921@icloud.com>
2024-03-17 17:36:34 +0300

    KS-4-3 created consts, used line breaks! Beans task

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,12 +1,21 @@
 package lesson4
 
+const val FAVORABLE_SUNNY_WEATHER = true
+const val TENT_OPENNESS = true
+const val FAVORABLE_AIR_HUMIDITY = 20
+const val FAVORABLE_SEASON = "не зима"
+
+
 fun main() {
     val isSunnyWeather = true
     val isTentOpen = true
-    val airHumidity = 20
-    val season = "зима"
+    val currentAirHumidity = 20
+    val currentSeason = "зима"
 
-    val isGoodConditionsForBeans = isSunnyWeather == true && isTentOpen == true && airHumidity == 20 && season != "зима"
+    val isGoodConditionsForBeans = isSunnyWeather == FAVORABLE_SUNNY_WEATHER &&
+            isTentOpen == TENT_OPENNESS &&
+            currentAirHumidity == FAVORABLE_AIR_HUMIDITY &&
+            currentSeason == FAVORABLE_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $isGoodConditionsForBeans")
 }
