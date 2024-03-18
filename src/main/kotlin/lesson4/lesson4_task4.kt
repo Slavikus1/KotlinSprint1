3acc5edbdf5cfa99995a448ad90786c743d969fe
Slavikus1 <vyacheslav921@icloud.com>
2024-03-18 19:28:01 +0300

    KS-4-4 used  !isOddDay

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,14 +2,13 @@ package lesson4
 
 fun main() {
     var numberOfWorkoutDay = 5
-    val isEvenDay = (numberOfWorkoutDay % 2) == 0
     val isOddDay = (numberOfWorkoutDay % 2) != 0
 
     println(
         """
         |Упражнения для рук: $isOddDay
-        |Упражнения для ног: $isEvenDay
-        |Упражнения для спины: $isEvenDay
+        |Упражнения для ног: ${!isOddDay}
+        |Упражнения для спины: ${!isOddDay}
         |Упражнения для пресса: $isOddDay
     """.trimMargin()
     )
