2967a45a0755a20223ee47128f1a106df8c7e705
Slavikus1 <vyacheslav921@icloud.com>
2024-03-18 12:25:37 +0300

    KS-4-4 refactoring workout progg

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -1,20 +1,16 @@
 package lesson4
 
 fun main() {
-    var numberOfWorkoutDay = 1
-    var absAndArmsExercises: Boolean
-    var backAndLegsExercises: Boolean
-
-    numberOfWorkoutDay += 4
-    absAndArmsExercises = numberOfWorkoutDay in (1..100).filter { number -> number % 2 != 0 }
-    backAndLegsExercises = numberOfWorkoutDay in (1..100).filter { number -> number % 2 == 0 }
+    var numberOfWorkoutDay = 5
+    val isEvenDay = (numberOfWorkoutDay % 2) == 0
+    val isOddDay = (numberOfWorkoutDay % 2) != 0
 
     println(
         """
-        |Упражнения для рук: $absAndArmsExercises
-        |Упражнения для ног: $backAndLegsExercises
-        |Упражнения для спины: $backAndLegsExercises
-        |Упражнения для пресса: $absAndArmsExercises
+        |Упражнения для рук: $isOddDay
+        |Упражнения для ног: $isEvenDay
+        |Упражнения для спины: $isEvenDay
+        |Упражнения для пресса: $isOddDay
     """.trimMargin()
     )
 }
