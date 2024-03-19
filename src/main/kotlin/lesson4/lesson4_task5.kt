8531661e13114139afd5d2d6e62e0395573b3644
Slavikus1 <vyacheslav921@icloud.com>
2024-03-19 18:08:48 +0300

    KS-4-5 refactoring ship task

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -3,36 +3,37 @@ package lesson4
 const val RECOMMENDED_NUMBER_OF_CREW = 70
 const val MINIMAL_NUMBER_OF_CREW = 55
 const val MINIMAL_NUMBER_OF_FOODBOX = 50
+const val SHIP_DAMAGED = false
+const val GOOD_WEATHER = true
 
 fun main() {
     println("Поврежден ли корпус? true/false")
 
-    val answerAboutDamage = readLine()
+    val answerAboutDamage = readln()
     val isCurrentDamage = answerAboutDamage.toBoolean()
 
     println("Укажите текущее количество членов экипажа:")
 
-    val answerAboutCrew = readLine()
-    val currentNumberOfCrew = answerAboutCrew!!.toInt()
+    val answerAboutCrew = readln()
+    val currentNumberOfCrew = answerAboutCrew.toInt()
 
     println("Укажите сколько ящиков с провизией на борту:")
 
-    val answerAboutFood = readLine()
-    val currentFood = answerAboutFood!!.toInt()
+    val answerAboutFood = readln()
+    val currentFood = answerAboutFood.toInt()
 
     println("Благоприятны ли метеусловия сейчас? true/false")
 
-    val answerAboutWeatherConditions = readLine()
+    val answerAboutWeatherConditions = readln()
     val isCurrentWeatherConditions = answerAboutWeatherConditions.toBoolean()
 
-    if ((isCurrentDamage == false) &&
+    if ((isCurrentDamage == SHIP_DAMAGED) &&
         (currentNumberOfCrew in (MINIMAL_NUMBER_OF_CREW..RECOMMENDED_NUMBER_OF_CREW)) &&
-        (currentFood >= MINIMAL_NUMBER_OF_FOODBOX) &&
-        (isCurrentWeatherConditions != true || isCurrentWeatherConditions == true)
+        (currentFood >= MINIMAL_NUMBER_OF_FOODBOX)
     ) println("Корабль может отправиться в плавание")
-    else if ((isCurrentDamage == true) &&
+    else if ((isCurrentDamage != SHIP_DAMAGED) &&
         (currentNumberOfCrew == RECOMMENDED_NUMBER_OF_CREW) &&
-        (isCurrentWeatherConditions == true) &&
+        (isCurrentWeatherConditions == GOOD_WEATHER) &&
         (currentFood >= MINIMAL_NUMBER_OF_FOODBOX)
     ) println("Корабль может отправиться в плавание")
     else println("Корабль не может отправиться в долгосрочное плавание")
