fc9bd5afa8e2dcb7290eb95226bf1de8251ccc39
Slavikus1 <vyacheslav921@icloud.com>
2024-03-22 17:01:59 +0300

    KS-7-5 random password generator

---
+++ src/main/kotlin/lesson7/lesson7_task5.kt
@@ -0,0 +1,36 @@
+package lesson7
+
+import kotlin.random.Random
+
+fun main() {
+    println("Введите число (не меньше 6), которое будет являться длинной пароля:")
+    var userPasswordSize = readln().toInt()
+    while (userPasswordSize < 6) {
+        println("Пожалуйста, введите число не меньше 6:")
+        userPasswordSize = readln().toInt()
+    }
+    val firstPasswordNumber = 1
+    val passwordSizeRange = firstPasswordNumber..userPasswordSize
+
+    val chars = 'a'..'z'
+    var charsList = chars.random().toString()
+
+    val upperChars = 'A'..'Z'
+    var upperCharsList = upperChars.random().toString()
+
+    val numbers = 1..9
+    var numbersList = numbers.random().toString()
+
+    var randomList = listOf(numbersList, charsList, upperCharsList)
+
+    var password = ""
+
+    for (i in passwordSizeRange) {
+        password += randomList.random()
+        charsList = chars.random().toString()
+        upperCharsList = upperChars.random().toString()
+        numbersList = numbers.random().toString()
+        randomList = listOf(charsList, upperCharsList, numbersList)
+    }
+    println("Ваш пароль: $password")
+}
