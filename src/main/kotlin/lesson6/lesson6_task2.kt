83ec5192783bb0f9aa895c6a3624232023580361
Slavikus1 <vyacheslav921@icloud.com>
2024-03-22 11:39:11 +0300

    KS-6-2 deleted circle, added Thread.sleep(var)

--- src/main/kotlin/lesson6/lesson6_task2.kt
+++ src/main/kotlin/lesson6/lesson6_task2.kt
@@ -4,10 +4,9 @@ fun main() {
     println("Добро пожаловать в таймер! Введите количество секунд, которые нужно засечь:")
     var seconds = readln().toInt()
     var resultSave = seconds
+    var delay = seconds * 1000
 
-    while (seconds > -1) {
-        println(seconds--)
-        Thread.sleep(1000)
-    }
+    Thread.sleep(delay.toLong())
+
     println("Прошло $resultSave секунд.")
 }
