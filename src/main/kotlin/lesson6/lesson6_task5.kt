e497de356d9b8098310dd85cd0adec14676164ef
Slavikus1 <vyacheslav921@icloud.com>
2024-03-22 12:30:16 +0300

    KS-6-5 refactoring anti-bot authorization task! Thanks for advice

--- src/main/kotlin/lesson6/lesson6_task5.kt
+++ src/main/kotlin/lesson6/lesson6_task5.kt
@@ -7,37 +7,18 @@ fun main() {
     var int2 = Random.nextInt(1, 9)
     var attempts = 3
 
-    while (attempts > 0) {
-        println("Требуется пройти проверку! Докажите что вы не бот - сложите $int1 и $int2:")
+    do {
+        println("Докажите что вы не бот! Сложите $int1 и $int2:")
         var result = readln().toInt()
 
         if (result == (int1 + int2)) {
             println("Добро пожаловать!")
             return
-        } else if (result != (int1 + int2)) {
-            println("Неверно! Осталось попыток ${--attempts}")
+        } else {
             int1 = Random.nextInt(1, 9)
             int2 = Random.nextInt(1, 9)
-            println("Попробуйте еще раз! Докажите что вы не бот - сложите $int1 и $int2:")
-            result = readln().toInt()
-            if (result == (int1 + int2)) {
-                println("Добро пожаловать!")
-                return
-            } else if (result != (int1 + int2)) {
-                println("Неверно! Осталось попыток ${--attempts}")
-                int1 = Random.nextInt(1, 9)
-                int2 = Random.nextInt(1, 9)
-                println("Попробуйте еще раз! Докажите что вы не бот - сложите $int1 и $int2:")
-                result = readln().toInt()
-                if (result == (int1 + int2)) {
-                    println("Добро пожаловать!")
-                    return
-                }
-                else  {
-                    println("Доступ запрещен")
-                    return
-                }
-            }
+            attempts--
         }
-    }
+    } while (attempts > 0)
+    println("Доступ запрещен!")
 }
