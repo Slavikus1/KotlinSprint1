6c4193555c726e6e310e1e91d5eb7760b4b199be
Slavikus1 <vyacheslav921@icloud.com>
2024-03-31 16:04:43 +0300

    KS-8-1 used summ. Thanks

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -2,10 +2,7 @@ package lesson8
 
 fun main() {
     val viewsArray = intArrayOf(1000, 4354, 2045, 9809, 6789, 9843, 6543)
-    var result: Int = 0
-    for (i in viewsArray){
-        result += i
-    }
+    val result = viewsArray.sum()
     println(result)
 }
 
