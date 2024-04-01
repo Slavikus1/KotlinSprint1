1949e5a320b1c1f6db2b5d269173bc330ab0db94
Slavikus1 <vyacheslav921@icloud.com>
2024-04-01 22:33:09 +0300

    KS-10-5 i love when i do smth interesting

---
+++ src/main/kotlin/lesson10/lesson10_task5.kt
@@ -0,0 +1,43 @@
+package lesson10
+
+const val LOGIN = "Slavikus1"
+const val PASSWORD = "4292xander"
+const val BASKET = "наушники, часы, будильник, весы"
+const val TOKEN_LENGTH = 32
+
+fun main() {
+    val token = authorization()
+    if (token == null) println("Authorization failed")
+    else getBasket(token = token)
+
+}
+
+fun authorization(): String? {
+    val chars = 'a'..'z'
+    val numbers = 0..9
+    val allChars = chars + numbers
+    val tokenGenerator = mutableListOf<String>()
+
+    tokenGenerator.add(chars.random().toString())
+    tokenGenerator.add(numbers.random().toString())
+    while (tokenGenerator.size != TOKEN_LENGTH) {
+        tokenGenerator.add(allChars.random().toString())
+    }
+    val token = tokenGenerator.joinToString("")
+
+    println("Введите логин:")
+    val login = readln()
+
+    println("Введите пароль:")
+    val password = readln()
+
+    return if (login == LOGIN && password == PASSWORD) {
+        token
+    } else {
+        null
+    }
+}
+
+fun getBasket(token: String) {
+    println(BASKET)
+}
