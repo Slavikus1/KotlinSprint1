0dc4d9a2faf82d4f4516416f4ab8524bf254050d
Slavikus1 <vyacheslav921@icloud.com>
2024-04-04 18:48:31 +0300

    KS-10-1; deleted one function

--- src/main/kotlin/lesson10/lesson10_task1.kt
+++ src/main/kotlin/lesson10/lesson10_task1.kt
@@ -3,22 +3,18 @@ package lesson10
 import kotlin.random.Random
 
 fun main() {
-    val humanResult = humanRollsTheDice()
-    val machineResult = machineRollsTheDice()
+    val humanResult = rollTheDice()
+    println("Человек выбросил $humanResult")
+
+    val machineResult = rollTheDice()
+    println("Машина выбросила $machineResult")
+
     if (humanResult > machineResult) println("Победило человечество")
     else if (machineResult > humanResult) println("Победили машины")
     else println("Ничья!")
 
 }
 
-fun humanRollsTheDice(): Int {
-    val result: Int = Random.nextInt(1, 6)
-    println("Человек сделал бросок и кубик показал: $result")
-    return result
-}
-
-fun machineRollsTheDice(): Int {
-    val result: Int = Random.nextInt(1, 6)
-    println("Машина сделала бросок и кубик показал: $result")
-    return result
+fun rollTheDice(): Int {
+    return Random.nextInt(1, 6)
 }
