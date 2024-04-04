4d42b2fba3318e190ce7b7a7b0e30bd9c94eeefb
Slavikus1 <vyacheslav921@icloud.com>
2024-04-04 21:53:54 +0300

    KS-11-1; создал класс User, описал свойства и вызвал два экземпляра класса в мейн

---
+++ src/main/kotlin/lesson11/lesson11_task1.kt
@@ -0,0 +1,25 @@
+package lesson11
+
+class User(
+    val login: String,
+    val password: String,
+    val email: String,
+    val uniqueId: String,
+){
+
+}
+fun main(){
+    val kolya = User("Kolya2", "123321", "kolya@mail.ru", "999")
+    println(kolya.login)
+    println(kolya.email)
+    println(kolya.password)
+    println(kolya.uniqueId)
+
+    println()
+
+    val petya = User("petr1", "321123", "petrfirst@mail.ru", "321")
+    println(petya.login)
+    println(petya.email)
+    println(petya.password)
+    println(petya.uniqueId)
+}
