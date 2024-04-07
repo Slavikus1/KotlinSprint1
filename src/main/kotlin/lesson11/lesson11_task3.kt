96bbb561c78ce46b7c40b945f487dd8a6e80cbfa
Slavikus1 <vyacheslav921@icloud.com>
2024-04-07 22:35:36 +0300

    KS-11-3; социальная сеть на минималках

---
+++ src/main/kotlin/lesson11/lesson11_task3.kt
@@ -0,0 +1,65 @@
+package lesson11
+
+class Room(
+    val cover: String,
+    val name: String,
+    val listOfMembers: MutableList<Member>,
+
+    ) {
+    fun addNewMember(newMember: Member) {
+        listOfMembers.add(newMember)
+    }
+
+    fun updateStatus(username: Member, newStatus: String) {
+        val name = username.nickname
+        username.status = newStatus
+        val status = username.status
+
+        println("Пользователь $name имеет новый статус: $status")
+
+    }
+
+    fun showInfo(avatar: Member) {
+        val minSec = 3
+        println("Нажмите на ${avatar.avatar} и удерживайте $minSec сек, чтобы получить инфо. Сколько секунд удерживаете?")
+        var answer = readln().toInt()
+        while (answer < minSec) {
+            Thread.sleep(answer.toLong())
+            println("Удерживайте не меньше трех секунд")
+            println("Сколько секунд удерживаете?")
+            answer = readln().toInt()
+            if (answer >= minSec) break
+        }
+        println(avatar.nickname)
+    }
+
+}
+
+class Member(
+    val avatar: String,
+    val nickname: String,
+    var status: String,
+)
+
+fun main() {
+    val dima = Member("Statham", "Dima21", "Микрофон выключен")
+    val vasya = Member("Stallone", "Vasyok321", "Разговаривает")
+    val sveta = Member("Svetka v Gelendzhe", "_Svetaaaa_", "Пользователь заглушен")
+
+    val room1 = Room("yellow, black and whilte", "MusicRoom", mutableListOf(dima, vasya))
+
+    println(room1.listOfMembers.size)
+
+    room1.addNewMember(sveta)
+
+    println(room1.listOfMembers.size)
+
+    room1.showInfo(dima)
+
+    println(vasya.status)
+
+    room1.updateStatus(vasya, "Микрофон выключен")
+
+    println(vasya.status)
+
+}
