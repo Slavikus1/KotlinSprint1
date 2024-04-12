f253338dc7b00b748ab981b87451fe48dcf1393c
Slavikus1 <vyacheslav921@icloud.com>
2024-04-12 12:46:25 +0300

    KS-11-4; скорректировал сущности для приложения рецептов

--- src/main/kotlin/lesson11/lesson11_task4.kt
+++ src/main/kotlin/lesson11/lesson11_task4.kt
@@ -3,29 +3,21 @@ package lesson11
 class RecipeCategory (
     val categoryName: String,
     val categoryDescription: String,
-    val categoryPhoto: String
+    val categoryPhoto: String,
+    val listOfRecipes: MutableList<Recipe>
 )
 
 class Recipe(
     val recipeName: String,
     val recipeDescription: String,
-    val ingredients: MutableList<String>,
+    val ingredients: MutableList<Ingredient>,
     val cookingTime: String,
 
 )
 
-class Ingredients(
+class Ingredient(
     val ingredientName: String,
     val ingredientAmount: Int,
     val measure: String,
     val ingredientToDo: String,
 )
-
-fun main(){
-    val burgersCategory = RecipeCategory("Бургеры", "Рецепты всех популярных видов бургеров",
-        "https/photo")
-    val cheeseburger = Recipe("Рецепт чизбургера", "Невероятно легкий рецепт вкусного чизбургера",
-        mutableListOf("Творог", "Куриное яйцо", "Пшеничная мука",
-        "Подсолнечное масло", "Сахар"), "30 минут")
-    val tomato = Ingredients("помидор", 1, "шт", "нарезать кольцами")
-}
