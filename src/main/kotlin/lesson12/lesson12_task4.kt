af67339f53c55a017d60b36ffc16e13005290a1e
Slavikus1 <vyacheslav921@icloud.com>
2024-04-25 18:43:53 +0300

    KS-12-4; блок init

---
+++ src/main/kotlin/lesson12/lesson12_task4.kt
@@ -0,0 +1,25 @@
+package lesson12
+
+const val KELVIN = 273
+
+class MyWeather(
+    _dayTemp: Int,
+    _nightTemp: Int,
+    _isPrecipitation: Boolean,
+) {
+    var dayTemp = _dayTemp - KELVIN
+    var nightTemp = _nightTemp - KELVIN
+    var isPrecipitation = _isPrecipitation
+
+    fun showInfo() {
+        println("Дневная темпертаура - $dayTemp, ночная температура - $nightTemp, осадки - $isPrecipitation")
+    }
+
+    init {
+        showInfo()
+    }
+}
+
+fun main() {
+    val newDay = MyWeather(5, 0, false)
+}
