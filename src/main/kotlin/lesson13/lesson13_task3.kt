02c81762005b098aa41586c29e819abc253f0c16
Slavikus1 <vyacheslav921@icloud.com>
2024-04-28 21:04:50 +0300

    KS-13-3; список компаний

--- src/main/kotlin/lesson13/lesson13_task3.kt
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -16,7 +16,7 @@ fun main() {
         DirectoryList("Semen", 89183133130),
         DirectoryList("Petr", 89180202022, "Google"),
         DirectoryList("Sam", 89181010101, "Microsoft"),
-        DirectoryList("Nicole", 89189090900,"null")
+        DirectoryList("Nicole", 89189090900, "null")
     )
     phoneBook.forEach { if (it.company != null) println(it.company) }
 }
