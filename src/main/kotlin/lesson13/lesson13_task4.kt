77b69e7f39c33582477b196292a1095599f7fe2d
Slavikus1 <vyacheslav921@icloud.com>
2024-04-30 21:53:09 +0300

    KS-13-4;

--- src/main/kotlin/lesson13/lesson13_task4.kt
+++ src/main/kotlin/lesson13/lesson13_task4.kt
@@ -1,37 +1,29 @@
 package lesson13
 
+import java.lang.IllegalArgumentException
+
 class DirectoryListBook(
     val name: String,
-    val phoneNumber: Long,
+    var phoneNumber: String?,
     var company: String? = null,
 ) {
+    init {
+        println("Введите номер телефона $name:")
+        phoneNumber = readlnOrNull()
+        phoneNumber?.toLong() ?: throw IllegalArgumentException ("Номер не указан. Такая запись не будет добавлена.")
+    }
+
     fun printData() {
         println("name - $name, phone number - $phoneNumber, company - $company")
     }
 }
 
 fun main() {
-    val phoneBook = mutableListOf<DirectoryListBook>()
-
-    println("Введите имя:")
-    val name = readln()
-
-    println("Введите номер телефона:")
-    val phoneNumber = readln()
-    val correctNumber = phoneNumber.toLongOrNull()
-
-    if (correctNumber == null) {
-        println("Номер телефона не указан, запись не будет добавлена.")
-        return
-    }
-
-    println("Введите название компании:")
-    val company = readlnOrNull()
-
-    val newContact = DirectoryListBook(name, correctNumber, company)
-    phoneBook.add(newContact)
-    newContact.printData()
-
+    val phoneBook = mutableListOf<DirectoryListBook>(
+        DirectoryListBook("Sam", "Ss",),
+        DirectoryListBook("Bob","s", "Apple")
+    )
+    phoneBook.forEach { it.printData() }
 
 }
 
