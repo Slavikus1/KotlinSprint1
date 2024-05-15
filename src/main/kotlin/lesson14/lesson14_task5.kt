c66471595db6174a12b395e98ab46b0e324d51e3
Slavikus1 <vyacheslav921@icloud.com>
2024-05-15 22:30:42 +0300

    KS-14-5; не понимаю как связать child id и парент id и сгруппировать их для печати

--- src/main/kotlin/lesson14/lesson14_task5.kt
+++ src/main/kotlin/lesson14/lesson14_task5.kt
@@ -1,19 +1,34 @@
 package lesson14
 
 class Chat(
-    private val messages: MutableList<Message> = mutableListOf(),
-    private val childMessages: MutableList<ChildMessage> = mutableListOf(),
+    val messages: MutableList<Message> = mutableListOf(),
 ) {
     fun addMessage(messageText: String, messageAuthor: String) {
-        messages.add(Message(messageText, messageAuthor, messageId = ))
+        messages.add(Message(messageText, messageAuthor, messageId = messages.size))
     }
 
-    fun addThreadMessage(parentMessageId: Int) {}
+    fun addThreadMessage(messageText: String, messageAuthor: String, parentMessageId: Int = messages.size) {
+        messages.add(ChildMessage(messageText, messageAuthor, messages.size, parentMessageId - 1))
+    }
 
-    fun printChat() {}
+    fun printChat() {
+        val groupById = messages.groupBy {it.messageId}
+    }
 }
 
 open class Message(val text: String, val author: String, var messageId: Int)
 
-class ChildMessage(text: String, author: String, childMessageId: Int, val parentMessageId: Int) :
-    Message(text, author, messageId = childMessageId)
+class ChildMessage(text: String, author: String, messageId: Int, val parentMessageId: Int) :
+    Message(text, author, messageId)
+
+fun main() {
+    val chat = Chat()
+    chat.addMessage("Всем здарова народ! Какие планы на майские?", "Паша")
+    chat.addThreadMessage("Здарова, братик. Я на шашлыки", "Митя")
+    chat.addMessage("Как проехать в галерею?", "Эмиль")
+    chat.addThreadMessage("на 10-ке", "Костя")
+
+    chat.messages.forEach { println(it.messageId) }
+
+    chat.printChat()
+}
