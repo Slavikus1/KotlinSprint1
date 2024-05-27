d08f775b609ae10b30248bf936f19be165e8f2e5
Slavikus1 <vyacheslav921@icloud.com>
2024-05-27 19:59:10 +0300

    KS-15-2; создал абстрактный класс, два класса потомка и реализацию сервера

---
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -0,0 +1,37 @@
+package lesson15
+
+abstract class WeatherStationStats() {
+    abstract val data: Int
+    abstract fun printInfo()
+}
+
+class Temperature(override var data: Int) : WeatherStationStats(){
+    override fun printInfo() {
+        println("Температура сегодня - $data")
+    }
+
+}
+
+class PrecipitationAmount(override val data: Int): WeatherStationStats(){
+    override fun printInfo() {
+        println("Осадки сегодня - $data")
+    }
+}
+
+class WeatherServer(val data: WeatherStationStats){
+    fun pushDataOnServer(){
+        if (data is Temperature){
+            println("Температура сегодня - ${data.data}")
+        }
+        else if (data is PrecipitationAmount){
+            println("Осадки сегодня - ${data.data}")
+        }
+    }
+}
+
+fun main(){
+    val precipitationMonday = PrecipitationAmount(10)
+    val temperatureMonday = Temperature(20)
+    val serverMonday = WeatherServer(temperatureMonday)
+    serverMonday.pushDataOnServer()
+}
