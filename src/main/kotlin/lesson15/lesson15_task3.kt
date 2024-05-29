be816f63712f2a68fd664416b7d3acb51dac1a4d
Slavikus1 <vyacheslav921@icloud.com>
2024-05-29 16:46:30 +0300

    KS-15-3; реализация метода в абстрактном классе

--- src/main/kotlin/lesson15/lesson15_task3.kt
+++ src/main/kotlin/lesson15/lesson15_task3.kt
@@ -2,22 +2,15 @@ package lesson15
 
 abstract class User {
     abstract val nickname: String
-    abstract fun writeMessage()
-
-}
-
-class CommonUser(override val nickname: String) : User() {
-    override fun writeMessage() {
+    fun writeMessage() {
         val message = readln()
         println(message)
     }
 }
+
+class CommonUser(override val nickname: String) : User()
 
 class Administrator(override val nickname: String) : User() {
-    override fun writeMessage() {
-        val message = readln()
-        println(message)
-    }
 
     fun deleteUser(user: User) {
         println("$nickname удалил пользователя $user")
