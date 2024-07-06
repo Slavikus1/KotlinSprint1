c3b58389816d34b948a08a6db8fdb7c4d97d8d9b
Slavikus1 <vyacheslav921@icloud.com>
2024-07-06 13:07:31 +0300

    KS-17-3; рефакторинг

--- src/main/kotlin/lesson17/lesson17_task3.kt
+++ src/main/kotlin/lesson17/lesson17_task3.kt
@@ -1,15 +1,19 @@
 package lesson17
 
-class Folder(){
-    var activeSecretFlag: Boolean = false
-    var name = ""
-        get() = if (activeSecretFlag) field = "скрытая папка" else field
-    var numberOfFiles = 0
-        get() = if (activeSecretFlag) field = 0 else field
+class Folder(
+    secret: Boolean,
+    title: String,
+    count: Int,
+) {
+    var activeSecretFlag = secret
+    var name = title
+        get() = if (activeSecretFlag) "скрытая папка" else field
+    var numberOfFiles = count
+        get() = if (activeSecretFlag) 0 else field
 }
 
-fun main(){
-    var folder = Folder()
+fun main() {
+    var folder = Folder(true, "Secret", 10)
     folder.activeSecretFlag = true
     println(folder.name)
     println(folder.numberOfFiles)
