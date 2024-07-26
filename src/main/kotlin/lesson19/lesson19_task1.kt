52220aa46949aedaf08c162cac5b460a03edde55
Slavikus1 <vyacheslav921@icloud.com>
2024-07-26 21:38:00 +0300

    KS-19-1; енам класс с перечислением рыб

--- src/main/kotlin/lesson19/lesson19_task1.kt
+++ src/main/kotlin/lesson19/lesson19_task1.kt
@@ -1,14 +1,14 @@
 package lesson19
 
-enum class Fishes{
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fishes(val type: String){
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main(){
     println("Пользователь может добавить в аквариум следующих рыб:")
     println()
-    for (i in Fishes.entries) println(i.name)
+    for (i in Fishes.entries) println(i.type)
 }
