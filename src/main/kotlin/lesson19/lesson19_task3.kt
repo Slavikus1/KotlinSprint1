cfc34b91d8803a3f3d107835a5a1ea9ab506e9ac
Slavikus1 <vyacheslav921@icloud.com>
2024-07-29 15:22:09 +0300

    KS-19-3; TODO

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,11 @@
+package lesson19
+
+class SpaceShip() {
+    fun takeOff() {}
+    fun land() {
+        TODO("Нужно чтобы этот метод позволял кораблю приземлиться")
+    }
+    fun shootAsteroid() {
+        TODO()
+    }
+}
