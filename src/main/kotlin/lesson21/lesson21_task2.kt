34f90a78d6cea4f483674121d336b91b20f741b8
Slavikus1 <vyacheslav921@icloud.com>
2024-08-09 13:05:59 +0300

    KS-21-2 использовал фильтр и сам

--- src/main/kotlin/lesson21/lesson21_task2.kt
+++ src/main/kotlin/lesson21/lesson21_task2.kt
@@ -1,9 +1,7 @@
 package lesson21
 
 fun MutableList<Int>.evenNumbersSum(numbers: MutableList<Int>): Int {
-    val evenList: MutableList<Int> = mutableListOf()
-    numbers.map { val result = it % 2; if (result == 0) evenList.add(it) }
-    return evenList.sum()
+    return this.filter { it % 2 == 0 }.sum()
 }
 
 fun main() {
