f2dafc0a868c984ae46153d8991696fa50d0c9d6
Slavikus1 <vyacheslav921@icloud.com>
2024-08-10 13:28:02 +0300

    KS-21-3 extension рефакторинг

--- src/main/kotlin/lesson21/lesson21_task2.kt
+++ src/main/kotlin/lesson21/lesson21_task2.kt
@@ -1,10 +1,10 @@
 package lesson21
 
-fun MutableList<Int>.evenNumbersSum(numbers: MutableList<Int>): Int {
+fun MutableList<Int>.evenNumbersSum(): Int {
     return this.filter { it % 2 == 0 }.sum()
 }
 
 fun main() {
     val a = mutableListOf(1, 2, 3, 4)
-    println(a.evenNumbersSum(a))
+    println(a.evenNumbersSum())
 }
